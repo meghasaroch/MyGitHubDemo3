b56a5037ca39159d2c78c38df7f2a7a4ff0896b8
coolamrinder999 <amrinder@vmmeducation.com>
2018-03-09 17:47:44 +0530

    I commited some changes(9 march,2018)

--- app/src/main/java/com/example/sony/mygithubdemo3/MainActivity.java
+++ app/src/main/java/com/example/sony/mygithubdemo3/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toast.makeText(this, "hello world", Toast.LENGTH_SHORT).show();
     }
 }
