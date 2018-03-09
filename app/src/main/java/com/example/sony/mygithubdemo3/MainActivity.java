c735d4af9e52c37f89bd2dfc04427d93adf14ad7
Dell <>
2018-03-09 17:55:45 +0530

    Testing Commit

--- app/src/main/java/com/example/sony/mygithubdemo3/MainActivity.java
+++ app/src/main/java/com/example/sony/mygithubdemo3/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.sony.mygithubdemo3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class
+MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         Toast.makeText(this, "hello world", Toast.LENGTH_SHORT).show();
+
+        Log.d("MYMessage","On Create Called");
     }
 }
